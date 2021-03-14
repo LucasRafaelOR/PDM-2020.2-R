324b8bcce18b48b68411368ed6a88a72b06cfac8
Lucas Rodrigues <lucasrafaelor@hotmail.com>
2021-03-14 20:56:46 -0300

    Atividade 5

---
+++ Semana_05/Atividade5/app/src/main/java/com/example/atividade5/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.atividade5
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgview = findViewById<ImageView>(R.id.imageView)
+        imgview.setOnClickListener { dispatchTakePictureIntent() }
+    }
+
+
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, 1)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("CODE", "Activity not found!")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+}
